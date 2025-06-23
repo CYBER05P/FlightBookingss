c1d8a8d73dda6ab2470e7143b97f5c7c0d697228
Chippychipssss <robertremy98@gmail.com>
2025-06-23 20:15:29 +0300

    Added About Us and Contact Us pages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         
       </Routes>
     </Router>
