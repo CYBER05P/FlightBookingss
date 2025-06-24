14c927e73424ded0ef285ace3541d551c4fb6cbf
Chippychipssss <robertremy98@gmail.com>
2025-06-24 16:46:29 +0300

    Re-added About and Contact pages with routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import HomePage from './pages/HomePage';
-
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/homepage" element={<HomePage />} />
-        
-        
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
